e1bc70e93585629c6020b40833673b1680dce1c3
Roberto Moreno <rampmaster@gmail.com>
2022-09-02 15:08:55 -0400

    fix: ajuste parcial de web

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", function () {
+    window.cookieconsent.initialise(json)
+});
+$(document).ready(function () {
+    $('.navbar-light .dmenu').hover(function () {
+        $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
+    }, function () {
+        $(this).find('.sm-menu').first().stop(true, true).slideUp(105)
+    });
+});
